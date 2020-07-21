79c3210b1a0bb9c264c889afa578fb0563c548c5
shers <shers.multimedia@gmail.com>
2020-07-21 11:35:24 -0300

    ejercicios del 7 al 10

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,17 +1,18 @@
 function mostrar()
-{
+{	
+	/*Al ingresar una edad menor a 18 años y
+	un estado civil distinto a "Soltero", mostrar el siguiente mensaje:
+	Es muy pequeño para NO ser soltero.'*/
+
 	//tomo la edad  
 	var edad;
-	var estadoCivil;
+	var estadoCivilIngresado;
 
 	edad=txtIdEdad.value;
-	estadoCivil=document.getElementById("estadoCivil").value;
+	estadoCivilIngresado=document.getElementById("estadoCivil").value;
 
-	if(edad<18 && estadoCivil!="Soltero")
+	if(edad<18 && estadoCivilIngresado!="Soltero")
 	{
 		alert("Es muy pequeño para NO ser soltero.")
 	}
-	
-
-
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,53 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+/*Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4	*/
 
+	//FORMA 1
+	//Genero el número RANDOM entre 1 y 10 
+	var nota;
+
+	//nota  toma una nota aleatoria
+	nota = Math.floor(Math.random() *10 +1);
+	
+	
+	if(nota>8) //Si la nota es menor a 8 salta el segundo if.
+	{
+		alert("EXCELENTE, su nota es " + nota + ".");
+	}else
+	{
+		if(nota>3) //Si la nota es menor a 3 actua else.
+		{
+			alert("APROBO, su nota es " + nota + ".");
+		}else
+		{
+			alert("Vamos, la proxima se puede. Su nota es " + nota + ".");
+		}
+	}
+
+	//FORMA 2
+	//Genero el número RANDOM entre 1 y 10 
+	var nota;
+	var resultado;
+
+	//nota  toma una nota aleatoria
+	nota = Math.floor(Math.random() *10 +1);
+
+	if(nota>8) //Si la nota es menor a 8 salta el segundo if.
+	{
+		resultado = "Excelente" ;
+	}else
+	{
+		if(nota>3) //Si la nota es menor a 3 actua else.
+		{
+			resultado = "Aprobado" ;
+
+		}else
+		{
+			resultado = "Vamos, la proxima se puede" ;
+		}
+	}
+	alert(resultado);
 }//FIN DE LA FUNCIÓN
